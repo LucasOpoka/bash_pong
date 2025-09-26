96f5fc96419d0f86535925c3c6136a3c40f2d5ee
Lucas Opoka <lucasopoka@icloud.com>
2025-09-26 19:57:38 +0300

    Major rework of the grid coordinate system, now each column is 2 char wide which simplifies the code by orders of magnitude

--- pong.sh
+++ pong.sh
@@ -2,33 +2,23 @@
 
 # -------------------------------------------------- Variables section --------------------------------------------------
 
+# Define pixel as 2 spaces
+pixel="  "
+
+# Get pong grid size, row height is 1 char and col width is 2 chars
+declare -i grid_rows=$(($(tput lines)-5))
+declare -i grid_cols=$((($(tput cols)-2)/2))
+
 # Wheter game is running
 declare -i game_running=0
 
-# Get pong grid size
-declare -i grid_rows=$(($(tput lines)-5))
-declare -i grid_cols=$(($(tput cols)-2))
-
-# Ensure grid_cols is even since both ball's width and horizontal step are 2
-if (( grid_cols % 2 == 1 )); then
-grid_cols=$(($grid_cols-1))
-fi
-
-# Ensure grid_rows is even since paddle's height is even and vertical step is 2
-if (( grid_rows % 2 == 1 )); then
-grid_rows=$(($grid_rows-1))
-fi
-
 # General paddle variables
 declare -i paddle_height=10
-declare -i paddle_width=2
+declare -i paddle_width=1
 declare -i paddle_speed=3
 
 # Get paddles' start position
 paddle_start_row=$(($grid_rows/2 - $paddle_height/2))
-if (( paddle_start_row % 2 == 1 )); then
-    paddle_start_row=$(($paddle_start_row-1))
-fi
 
 # Left paddle variables
 declare -i left_paddle_row=$paddle_start_row
@@ -38,20 +28,15 @@ declare -i left_paddle_col=0
 # Right paddle variables
 declare -i right_paddle_row=$paddle_start_row
 declare -i new_right_paddle_row=$right_paddle_row
-declare -i right_paddle_col=$(($grid_cols - 2))
+declare -i right_paddle_col=$(($grid_cols - 1))
 
 # Ball variables
 declare -i  ball_height=1
-declare -i  ball_width=2
+declare -i  ball_width=1
 declare -i  ball_row=$(($grid_rows/2 - 2))
 declare -i  ball_col=$(($grid_cols/2))
 declare -i  ball_step_x=-1
-declare -i  ball_step_y=2
-
-# Ensure starting ball col is even
-if (( ball_col % 2 == 1 )); then
-ball_col=$(($ball_col-1))
-fi
+declare -i  ball_step_y=1
 
 # Internal field separator, how bash splits strings
 IFS=''
@@ -77,7 +62,7 @@ init_game() {
     stty -echo
     for ((i=0; i<grid_rows; i++)); do
         for ((j=0; j<grid_cols; j++)); do
-            eval "arr$i[$j]=' '"
+            eval "arr$i[$j]=\"$pixel\""
         done
     done
 }
@@ -95,7 +80,7 @@ draw_rectangle()
 
     for ((i=0; i<$height; i++)); do
         for ((j=0; j<$width; j++)); do
-            eval "arr$(($1+$i))[$(($2+$j))]=\"${3} ${4}\""
+            eval "arr$(($1+$i))[$(($2+$j))]=\"${3}$pixel${4}\""
         done
     done
 }
@@ -118,7 +103,7 @@ detect_ball_x_paddle_colision()
     local position;
 
     eval "pos=\${arr$(($1))[$(($2))]}"
-    if [ "$pos" == "$border_color $no_color" ]; then
+    if [ "$pos" == "$border_color$pixel$no_color" ]; then
         return 0
     fi
 
@@ -126,24 +111,28 @@ detect_ball_x_paddle_colision()
 }
 
 
-draw_board() {
+draw_grid()
+{
+    # Top row
     move_and_draw 1 1 "$border_color+$no_color"
-    for ((i=2; i<=grid_cols+1; i++)); do
-        move_and_draw 1 $i "$border_color-$no_color"
+    for ((i=1; i<=grid_cols; i++)); do
+        move_and_draw 1 $(($i*2)) "$border_color--$no_color"
     done
-    move_and_draw 1 $((grid_cols + 2)) "$border_color+$no_color"
+    move_and_draw 1 $((grid_cols*2 + 2)) "$border_color+$no_color"
 
+    # Middle rows
     for ((i=0; i<grid_rows; i++)); do
         move_and_draw $((i+2)) 1 "$border_color|$no_color"
         eval printf "\"\${arr$i[*]}\""
         printf "$border_color|$no_color"
     done
 
+    # Bottom row
     move_and_draw $((grid_rows+2)) 1 "$border_color+$no_color"
-    for ((i=2; i<=grid_cols+1; i++)); do
-        move_and_draw $((grid_rows+2)) $i "$border_color-$no_color"
+    for ((i=1; i<=grid_cols; i++)); do
+        move_and_draw $((grid_rows+2)) $(($i*2)) "$border_color--$no_color"
     done
-    move_and_draw $((grid_rows+2)) $((grid_cols + 2)) "$border_color+$no_color"
+    move_and_draw $((grid_rows+2)) $((grid_cols*2 + 2)) "$border_color+$no_color"
 }
 
 
@@ -171,7 +160,8 @@ move_right_paddle()
 }
 
 
-move_paddles() {
+move_paddles()
+{
     # Left paddle
     draw_paddle "$left_paddle_row" "$left_paddle_col" "$no_color" "$no_color"
     left_paddle_row=$new_left_paddle_row
@@ -230,9 +220,9 @@ get_user_input()
                     ;;
             's')    kill -$SIG_LEFT_DOWN $game_pid  # s
                     ;;
-            'o')    kill -$SIG_RIGHT_UP $game_pid    # o
+            'o')    kill -$SIG_RIGHT_UP $game_pid   # o
                     ;;
-            'l')    kill -$SIG_RIGHT_DOWN $game_pid  # l
+            'l')    kill -$SIG_RIGHT_DOWN $game_pid # l
                     ;; 
         esac
     done
@@ -244,34 +234,33 @@ draw_start_message()
     local mid_col=$(($grid_cols / 2))
     declare -i str_col
 
-    eval "str_arr0=(-22 -20 -18 -16 -12 -6 -2 0 2 6 8 10 12 16 18 20)"
-    eval "str_arr1=(-22 -12 -10 -6 0 6 16 22)"
-    eval "str_arr2=(-22 -20 -18 -12 -8 -6 0 6 8 10 16 18 20)"
-    eval "str_arr3=(-22 -12 -6 0 6 16 22)"
-    eval "str_arr4=(-22 -20 -18 -16 -12 -6 0 6 8 10 12 16 22)"
+    eval "str_arr0=(-11 -10 -9 -8 -6 -3 -1 0 1 3 4 5 6 8 9 10)"
+    eval "str_arr1=(-11 -6 -5 -3 0 3 8 11)"
+    eval "str_arr2=(-11 -10 -9 -6 -4 -3 0 3 4 5 8 9 10)"
+    eval "str_arr3=(-11 -6 -3 0 3 8 11)"
+    eval "str_arr4=(-11 -10 -9 -8 -6 -3 0 3 4 5 6 8 11)"
     eval "str_arr5=()"
-    eval "str_arr6=(-6 -4 -2 4 6)"
-    eval "str_arr7=(-4 2 8)"
-    eval "str_arr8=(-4 2 8)"
-    eval "str_arr9=(-4 2 8)"
-    eval "str_arr10=(-4 4 6)"
+    eval "str_arr6=(-3 -2 -1 2 3)"
+    eval "str_arr7=(-2 1 4)"
+    eval "str_arr8=(-2 1 4)"
+    eval "str_arr9=(-2 1 4)"
+    eval "str_arr10=(-2 2 3)"
     eval "str_arr11=()"
-    eval "str_arr12=(-16 -18 -20 -12 -10 -8 -2 0 6 8 10 16 18 20)"
-    eval "str_arr13=(-22 -10 -4 2 6 12 18)"
-    eval "str_arr14=(-18 -20 -10 -4 -2 0 2 6 8 10 18)"
-    eval "str_arr15=(-16 -10 -4 2 6 12 18)"
-    eval "str_arr16=(-18 -20 -22 -10 -4 2 6 12 18)"
+    eval "str_arr12=(-8 -9 -10 -6 -5 -4 -1 0 3 4 5 8 9 10)"
+    eval "str_arr13=(-11 -5 -2 1 3 6 9)"
+    eval "str_arr14=(-9 -10 -5 -2 -1 0 1 3 4 5 9)"
+    eval "str_arr15=(-8 -5 -2 1 3 6 9)"
+    eval "str_arr16=(-9 -10 -11 -5 -2 1 3 6 9)"
 
     for ((i=0; i<=16; i++)); do
         eval "sub_arr_len=\${#str_arr$i[@]}"
         for ((j=0; j<sub_arr_len; j++)); do
             eval "str_col=\$((str_arr$i[$j]))";
-            eval "arr$(($mid_row+$i-8))[$(($mid_col+$str_col))]=\"\$1 \$no_color\"";
-            eval "arr$(($mid_row+$i-8))[$(($mid_col+$str_col+1))]=\"\$1 \$no_color\"";
+            eval "arr$(($mid_row+$i-8))[$(($mid_col+$str_col))]=\"\$1\$pixel\$no_color\"";
         done
     done
     
-    draw_board
+    draw_grid
 }
 
 start_screen_loop()
@@ -318,7 +307,7 @@ game_loop()
     do
         move_paddles
         move_ball
-        draw_board
+        draw_grid
         sleep 0.05
     done
 
@@ -337,7 +326,7 @@ clear_game()
 # -------------------------------------------------- Main section --------------------------------------------------
 
 init_game
-draw_board
+draw_grid
 
 start_screen_loop
 game_loop & game_pid=$!
